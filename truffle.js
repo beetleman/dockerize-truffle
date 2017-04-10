6b1ce41069f09ad02e1a1ddfd316517b19aa2948
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2017-04-10 13:50:53 +0200

    init

---
+++ truffle.js
@@ -0,0 +1,12 @@
+const HOST = process.env.RPC_HOST || 'localhost';
+const PORT = process.env.RPC_PORT || 8545;
+
+module.exports = {
+    networks: {
+        development: {
+            host: HOST,
+            port: PORT,
+            network_id: "*" // Match any network id
+        }
+    }
+};
